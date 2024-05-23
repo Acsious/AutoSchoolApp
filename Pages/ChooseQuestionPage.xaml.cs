cc1436c094d3e68aed5a3eac87114fc861c85360
Acsious <ulchenko.f@mail.ru>
2024-05-24 01:26:10 +0300

    choose pages

--- Pages/ChooseQuestionPage.xaml.cs
+++ Pages/ChooseQuestionPage.xaml.cs
@@ -6,4 +6,134 @@ public partial class ChooseQuestionPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_1(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_2(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_3(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_4(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_5(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_6(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_7(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_8(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_9(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_10(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_11(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_12(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_13(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_14(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_15(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_16(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_17(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_18(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_19(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_20(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_21(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_22(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_23(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_24(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_25(object sender, TappedEventArgs e)
+    {
+
+    }
 }
--- Pages/ChooseTicketPage.xaml.cs
+++ Pages/ChooseTicketPage.xaml.cs
@@ -6,4 +6,204 @@ public partial class ChooseTicketPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_1(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_2(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_3(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_4(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_5(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_6(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_7(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_8(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_9(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_10(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_11(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_12(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_13(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_14(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_15(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_16(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_17(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_18(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_19(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_20(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_21(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_22(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_23(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_24(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_25(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_26(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_27(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_28(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_29(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_30(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_31(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_32(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_33(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_34(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_35(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_36(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_37(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_38(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_39(object sender, TappedEventArgs e)
+    {
+
+    }
 }
